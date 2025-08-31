3d26b235bf66083d424835dbc120cf74b87a9ddc
dwiaziprasetya <dwiaziprasetya456@gmail.com>
2025-08-31 17:31:57 +0700

    initial commit

---
+++ composeApp/src/commonTest/kotlin/com/dwiaziprasetya/spotifycmp/ComposeAppCommonTest.kt
@@ -0,0 +1,12 @@
+package com.dwiaziprasetya.spotifycmp
+
+import kotlin.test.Test
+import kotlin.test.assertEquals
+
+class ComposeAppCommonTest {
+
+    @Test
+    fun example() {
+        assertEquals(3, 1 + 2)
+    }
+}
---
+++ composeApp/src/iosMain/kotlin/com/dwiaziprasetya/spotifycmp/MainViewController.kt
@@ -0,0 +1,5 @@
+package com.dwiaziprasetya.spotifycmp
+
+import androidx.compose.ui.window.ComposeUIViewController
+
+fun MainViewController() = ComposeUIViewController { App() }
